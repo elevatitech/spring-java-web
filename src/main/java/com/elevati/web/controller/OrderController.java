d6ed796f19378328b1946107f71e92151b0787f3
Salil Kothadia <salil@salil.dev>
2023-07-16 10:53:53 +0530

    Sync Data

--- src/main/java/com/elevati/web/controller/OrderController.java
+++ src/main/java/com/elevati/web/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.elevati.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(path = "/order")
@@ -10,8 +11,17 @@ public class OrderController
 {
 
     @GetMapping
-    public String getAllOrders()
+    public String index()
     {
         return "order";  // order.html view file
+    }
+
+    @GetMapping(path = "/sync")
+    public ModelAndView sync()
+    {
+        // TODO: Fetch Orders from sample Shopify api response available at the below endpoint https://elevatitech.com/api/test/shopify/orders
+        // TODO: And Persist orders in a MySQL DB
+
+        return new ModelAndView("redirect:/");
     }
 }
